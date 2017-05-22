90aabb3028fb7a7f5159b464c872a8e4700f28b0
Minh Hieu <cuti.it@hotmail.com>
2017-05-22 10:22:20 +0700

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #router to controller hello_world
+  get 'hello/index'
+end
